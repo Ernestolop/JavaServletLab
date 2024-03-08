97fa8dde766266b626387d8775d2f73300ed5c47
Ernesto Daniel Lopez Deggeller <ernesto.lopez@clt.com.py>
2024-03-08 15:38:36 -0300

    feat: post json class content

--- src/main/java/org/elopez/webapp/servlet/controllers/ProductJsonServlet.java
+++ src/main/java/org/elopez/webapp/servlet/controllers/ProductJsonServlet.java
@@ -1,11 +1,13 @@
 package org.elopez.webapp.servlet.controllers;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 import org.elopez.webapp.servlet.models.Product;
 import org.elopez.webapp.servlet.services.ProductServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -21,5 +23,21 @@ public class ProductJsonServlet extends HttpServlet {
         ObjectMapper mapper = new ObjectMapper();
         String json = mapper.writeValueAsString(products);
         response.getWriter().write(json);
+    }
+
+    @Override 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        ServletInputStream jsonStream = request.getInputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        Product product = mapper.readValue(jsonStream, Product.class);
+
+        response.setContentType("text/html");
+        try(PrintWriter out = response.getWriter()) {
+            out.println("<h1>Product</h1>");
+            out.println("<p>Id: " + product.getId() + "</p>");
+            out.println("<p>Name: " + product.getName() + "</p>");
+            out.println("<p>Type: " + product.getType() + "</p>");
+            out.println("<p>Price: " + product.getPrice() + "</p>");
+        }
     }
 }
